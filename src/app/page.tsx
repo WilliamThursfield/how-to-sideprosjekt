00770812381c35c51c97fba03ccbc92a1367d497
William Thursfield <withu4688@uib.no>
2024-03-20 17:20:34 +0100

    bla bla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-full gap-10 p-24">
-      <h1 className="text-3xl font-medium">
-        Velkommen til {'"'}How to sideprosjekt{'"'}
+    <div>
+      <h1>
+        Velkommen til {'"'}William sin nettside{'"'}
       </h1>
-
       <p>
-        Om du vil ha oppgaver, les{" "}
-        <span className="bg-gray-200 font-mono py-1 px-2 rounded-md">
-          README.md
-        </span>
-        .
+        Jeg går datasikkerhet på Universitetet i Bergen. 
+        Henrik er en tulling!
       </p>
-    </main>
+    </div>
   );
 }
