1c7ad8ecc8036545cced1f1a5e98b20bd88688f5
William Thursfield <withu4688@uib.no>
2024-03-20 17:26:47 +0100

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div> className="text-center"
-      <h1>className="text-2xl
+    <div className="text-center"> 
+      <h1 className="text-2xl">
         Velkommen til {'"'}William sin nettside{'"'}
       </h1>
       <p>
         Jeg går datasikkerhet på Universitetet i Bergen. 
-        Henrik er en tulling!
+        
       </p>
     </div>
   );
